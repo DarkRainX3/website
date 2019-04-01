a6ecf76551cb00ba79196ef926bafb152214b573
Waley <44916399+DarkRainX3@users.noreply.github.com>
2019-04-01 00:43:36 -0600

    updates

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import RegForm
 # Create your views here.
 def home(request):
     #communicate with django database queries here
@@ -8,5 +10,17 @@ def home(request):
 
     args = {'myName': name, 'numbers':numbers}
     return render(request,'accounts/home.html', args)
+
 def new(request):
     return render(request,'accounts/new.html')
+
+def register(request):
+    if request.method=="POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/home')
+    else:
+        form = UserCreationForm()
+        args = {'form':form}
+        return render(request, 'accounts/register.html',args)
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 from django.contrib.auth.views import LoginView
+from mysite import views
 
 urlpatterns = [
+    url(r'^$', views.home_redirect, name = 'home_redirect'),
     path('admin/', admin.site.urls),
     url(r'^home/', include('accounts.urls')),
-    url(r'^login/$', LoginView.as_view(template_name='accounts/login.html'),name='login')
+    url(r'^login/$', LoginView.as_view(template_name='accounts/login.html'),name='login'),
     # url(r'^home/$', LoginView.as_view(template_name='accounts/home.html'),name='home')
     # url(r'^new/$', views.new)
 ]
